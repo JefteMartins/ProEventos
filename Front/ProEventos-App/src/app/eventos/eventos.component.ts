35591564c2df3895b1ec2bde3a7f42bf7e5d9b65
JefteMartins <jefte.martins@hotmail.com>
2023-05-23 17:22:54 -0300

    adicionando ngfor

--- Front/ProEventos-App/src/app/eventos/eventos.component.ts
+++ Front/ProEventos-App/src/app/eventos/eventos.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component,OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-eventos',
   templateUrl: './eventos.component.html',
   styleUrls: ['./eventos.component.scss']
 })
-export class EventosComponent {
+
+export class EventosComponent implements  OnInit {
+
+  public eventos: any = [
+    {
+      Tema: 'Angular 11',
+      Local: 'Belo Horizonte'
+    },
+    {
+      Tema: 'Angular 12',
+      Local: 'Teste'
+    },
+    {
+      Tema: 'Angular 13',
+      Local: 'Teste 2'
+    },
+  ]
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+  }
 
 }
