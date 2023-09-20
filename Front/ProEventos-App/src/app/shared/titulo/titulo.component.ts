0ce42c86f0b44b9b9b4c5fc2f377e83ae12317e9
Jefte Martins <jefte.martins@hotmail.com>
2023-09-19 23:50:27 -0300

    atualização do componente de eventos

--- Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
+++ Front/ProEventos-App/src/app/shared/titulo/titulo.component.ts
@@ -3,10 +3,13 @@ import { Component, Input, OnInit } from '@angular/core';
 @Component({
   selector: 'app-titulo',
   templateUrl: './titulo.component.html',
-  styleUrls: ['./titulo.component.css']
+  styleUrls: ['./titulo.component.scss']
 })
 export class TituloComponent implements OnInit {
   @Input() titulo= '';
+  @Input() iconClass= 'fa fa-user';
+  @Input() subtitulo= 'Desde 2023';
+  @Input() botaoListar= false;
   constructor() { }
 
   ngOnInit() {
