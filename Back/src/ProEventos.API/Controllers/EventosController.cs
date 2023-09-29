b3b14beb5d9a0a1ff1b4bbf13823e5133e2c381a
Jefte Martins <jefte.martins@hotmail.com>
2023-09-29 20:34:52 -0300

    primeiro Dto

--- Back/src/ProEventos.API/Controllers/EventosController.cs
+++ Back/src/ProEventos.API/Controllers/EventosController.cs
@@ -9,6 +9,7 @@ using ProEventos.Domain;
 using ProEventos.Persistence.Contextos;
 using ProEventos.Application.Contratos;
 using Microsoft.AspNetCore.Http;
+using ProEventos.API.Dtos;
 
 namespace ProEventos.API.Controllers
 {
@@ -31,7 +32,24 @@ namespace ProEventos.API.Controllers
                 var eventos = await _eventoService.GetAllEventosAsync(true);
                 if (eventos == null) return NotFound("Nenhum evento encontrado.");
 
-                return Ok(eventos);
+                var eventoRetorno = new List<EventoDto>();
+
+                foreach (var evento in eventos)
+                {
+                    eventoRetorno.Add(new EventoDto()
+                    {
+                        Id = evento.Id,
+                        Local = evento.Local,
+                        DataEvento = evento.DataEvento.ToString(),
+                        Tema = evento.Tema,
+                        QtdPessoas = evento.QtdPessoas,
+                        ImagemURL = evento.ImagemURL,
+                        Telefone = evento.Telefone,
+                        Email = evento.Email
+                    });
+                }
+
+                return Ok(eventoRetorno);
             }
             catch (Exception ex)
             {
