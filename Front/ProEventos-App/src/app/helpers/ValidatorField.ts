3480f805bc12a0f2a77276c422ff506074534fd3
Jefte Martins <jefte.martins@hotmail.com>
2023-09-28 21:05:59 -0300

    finalizando validação do registro

---
+++ Front/ProEventos-App/src/app/helpers/ValidatorField.ts
@@ -0,0 +1,23 @@
+import { FormGroup, AbstractControl } from '@angular/forms';
+
+export class ValidatorField {
+  static MustMatch(controlName: string, matchingControlName: string): any {
+    return (group: AbstractControl) => {
+      const formGroup = group as FormGroup;
+      const control = formGroup.controls[controlName];
+      const matchingControl = formGroup.controls[matchingControlName];
+
+      if (matchingControl.errors && !matchingControl.errors['MustMatch']) {
+        return null;
+      }
+
+      if (control.value !== matchingControl.value) {
+        matchingControl.setErrors({ mustMatch: true });
+      } else {
+        matchingControl.setErrors(null);
+      }
+
+      return null;
+    };
+  }
+}
